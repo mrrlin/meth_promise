baee61eb1199b7ea2946177fe09321209e04eff5
Angelina <angelina.kulyaeva@yandex.ru>
2024-02-17 23:06:18 +0300

    Start

---
+++ lesson-Promise/script/renderPage.js
@@ -0,0 +1,10 @@
+import fetchRequest from './fetchRequest.js';
+import renderGoods from './renderGoods.js';
+import renderDiscount from './renderDiscount.js';
+import renderSelect from './renderSelect.js';
+import preload from './preload.js';
+
+const wrapperGoods = document.querySelector('.wrapper_goods');
+const wrapperDiscount = document.querySelector('.wrapper_discount');
+const wrapperSelect = document.querySelector('.wrapper_select');
+
